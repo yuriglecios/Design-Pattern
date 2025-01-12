5d19974ef703488b5be42c2fdc2b1e89ffe83a47
yuri glecios <glleciosyuri@gmail.com>
2025-01-12 19:05:25 -0300

    Create project

---
+++ src/test/java/br/com/pattern/DesignPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
